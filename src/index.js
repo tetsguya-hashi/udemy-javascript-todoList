e199fcfe493733bcf0ccddecb18105644bc53451
tetsguya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-25 17:42:25 +0900

    myself-code add-bfore

--- src/index.js
+++ src/index.js
@@ -12,9 +12,24 @@ const onClickAdd = () => {
   //button(完了)ボタンの生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const div = document.createElement("div");
+    const conpleteList = document
+      .getElementById("complete-list")
+      .appendChild(div);
+
+    console.log(conpleteList);
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("inconmplete-list").removeChild(deleteTarget);
+  });
   //button(削除)ボタンの生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //親要素を削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("inconmplete-list").removeChild(deleteTarget);
+  });
   //divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
