d1db164bdfc4f8e05376c4a797825485fd1b1de5
tetsguya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-25 16:56:28 +0900

    未完了のTODOへテキストを追加

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const addText = document.getElementById("add-text");
 const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化
   const inputText = addText.value;
   addText.value = "";
-  alert(inputText);
+  //divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  //ulタグの子要素にdiv
+  document.getElementById("inconmplete-list").appendChild(div);
 };
 
 document
