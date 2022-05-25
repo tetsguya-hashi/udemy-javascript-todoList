5fd3f976afb64531da20b3b97f1df51f31a99fe3
tetsguya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-25 17:05:15 +0900

    未完了のリストへボタンの追加

--- src/index.js
+++ src/index.js
@@ -9,8 +9,16 @@ const onClickAdd = () => {
   //liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
+  //button(完了)ボタンの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  //button(削除)ボタンの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   //ulタグの子要素にdiv
   document.getElementById("inconmplete-list").appendChild(div);
 };
