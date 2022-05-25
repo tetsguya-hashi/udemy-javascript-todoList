031b713ed7057da6b788d258688098fcf1fc3100
tetsguya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-25 16:41:22 +0900

    テキストボックスの値を取得し、初期化

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+const addText = document.getElementById("add-text");
+const onClickAdd = () => {
+  const inputText = addText.value;
+  addText.value = "";
+  alert(inputText);
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
